adaca91612f8f6e4405eeffce6e687d688aa7a77
mariamjaludi <mariam.jaludi@gmail.com>
2019-09-07 18:34:49 -0700

    polygons added

--- map1.py
+++ map1.py
@@ -1,5 +1,6 @@
 import folium
 import pandas
+import json
 
 # import data from Volcanoes.txt
 data = pandas.read_csv("Volcanoes.txt")
@@ -32,7 +33,11 @@ def color_assign(elev):
 for lt, ln, elev, name in zip(lat, lon, elevation, name):
     # assign a point for each volcano
     iframe = folium.IFrame(html=html % (name, name, elev), width=200, height=100)
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color=color_assign(elev))))
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=folium.Popup(iframe), fill_color=color_assign(elev), color = 'grey', fill = True, fill_opacity=0.7))
+
+# add json data
+fg.add_child(folium.GeoJson(data=json.load(open('world.json', 'r', encoding='utf-8-sig')),
+style_function=lambda x: {'fillColor':'yellow'}))
 
 map.add_child(fg)
 map.save("Map1.html")
