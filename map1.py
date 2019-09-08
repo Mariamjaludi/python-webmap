ec2e2d31c3422a05c4ca4bde9e310bde513b5d7a
mariamjaludi <mariam.jaludi@gmail.com>
2019-09-07 17:28:14 -0700

    volcanoes from file mapped

--- map1.py
+++ map1.py
@@ -1,8 +1,21 @@
 import folium
-map = folium.Map(location=[51.521925,-0.162761], zoom_start=12, tiles="Stamen Terrain")
+import pandas
+
+# import data from Volcanoes.txt
+data = pandas.read_csv("Volcanoes.txt")
+
+# extract latitude and longtitude into separate lists:
+lat = list(data['LAT'])
+lon = list(data['LON'])
+
+# create map object
+map = folium.Map(location=[37.757536,-122.446517], zoom_start=12, tiles="Stamen Terrain")
 fg =folium.FeatureGroup(name="My Map")
 
-for coordinates in [[51.521925,-0.162761], [51.521925,0.162761]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="Hi, I am a Marker", icon=folium.Icon(color="green")))
+# use zip to iterate through both lists at the same time.
+for lt, ln in zip(lat, lon):
+    # assign a point for each volcano
+    fg.add_child(folium.Marker(location=[lt, ln], popup="Hi, I am a volcano", icon=folium.Icon(color="green")))
+
 map.add_child(fg)
 map.save("Map1.html")
