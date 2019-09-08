4418eb239a622c4f8dfaddda966a48fc5383892b
mariamjaludi <mariam.jaludi@gmail.com>
2019-09-07 17:59:33 -0700

    points dynamically colored based on elevation

--- map1.py
+++ map1.py
@@ -18,11 +18,21 @@ html = """<h4>Volcano Information:</h4>
 <strong><a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a></strong><br>
 Elevation: %s m
 """
+
+# function to assign a color to an elevation range
+def color_assign(elev):
+    if elev < 1000:
+        return "green"
+    elif 1000 <= elev < 3000:
+        return "orange"
+    else:
+        return "red"
+
 # use zip to iterate through multiple lists at the same time.
 for lt, ln, elev, name in zip(lat, lon, elevation, name):
     # assign a point for each volcano
     iframe = folium.IFrame(html=html % (name, name, elev), width=200, height=100)
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color="red")))
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color=color_assign(elev))))
 
 map.add_child(fg)
 map.save("Map1.html")
