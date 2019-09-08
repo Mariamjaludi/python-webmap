c849c30794b730630fa06f5c01470a4e5a57d670
mariamjaludi <mariam.jaludi@gmail.com>
2019-09-07 17:02:57 -0700

    adding multiple points

--- map1.py
+++ map1.py
@@ -2,6 +2,7 @@ import folium
 map = folium.Map(location=[51.521925,-0.162761], zoom_start=12, tiles="Stamen Terrain")
 fg =folium.FeatureGroup(name="My Map")
 
-fg.add_child(folium.Marker(location=[51.5, -.16], popup="Hi, I am a Marker", icon=folium.Icon(color="green")))
+for coordinates in [[51.521925,-0.162761], [51.521925,0.162761]]:
+    fg.add_child(folium.Marker(location=coordinates, popup="Hi, I am a Marker", icon=folium.Icon(color="green")))
 map.add_child(fg)
 map.save("Map1.html")
